a9aae0b52c3cf2072b681e15fe2a8db3ddadcb6e
DCTP-BE <akpata.thelma@gmail.com>
2024-06-10 23:09:11 +0100

    socket

--- chat-app-backend/server/server.js
+++ chat-app-backend/server/server.js
@@ -9,7 +9,6 @@ const io = require("socket.io")(server);
 
 io.on("connection", (socket) => {
     console.log("New client connected");
-})
 
 socket.on("disconnect", () => {
     console.log("Client disconnected");
