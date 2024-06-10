4ae6ac2651a69b0cb8a36d8d04933d8e6f30f408
DCTP-BE <akpata.thelma@gmail.com>
2024-06-10 20:06:12 +0100

    server

--- chat-app-backend/server/server.js
+++ chat-app-backend/server/server.js
@@ -7,6 +7,12 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
+io.on("connection", (socket) => {
+    console.log("New client connected");
+})
+
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
