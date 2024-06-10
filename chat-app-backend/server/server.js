955b7d3fd4f2a9e2157fb6cd996c2c9dc4e7ddd5
DCTP-BE <akpata.thelma@gmail.com>
2024-06-10 20:26:25 +0100

    server config

--- chat-app-backend/server/server.js
+++ chat-app-backend/server/server.js
@@ -19,12 +19,16 @@ socket.on("message", (message) => {
     io.emit("message", message);
 })
 
-mongoose.connect("mongodb://localhost:27017/chat-app", { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+// mongoose.connect("mongodb://localhost:27017/chat-app", { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+//     console.log("MongoDB connected");
+// })
+
+mongoose.connect("mongodb://cluster0.lssixvh.mongodb.net/wire-chat-app", { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log("MongoDB connected");
-})
+ })
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
