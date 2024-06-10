442cde8536ac72669227e7f27d4fd159478ea9a4
DCTP-BE <akpata.thelma@gmail.com>
2024-06-10 20:02:19 +0100

    socket config

--- chat-app-backend/server/server.js
+++ chat-app-backend/server/server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 
 const app = express();
 const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
 const PORT = process.env.PORT || 5000;
 
