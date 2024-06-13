62cc1b214b023cfee130131b1b63e82464cfdfb0
DCTP-BE <akpata.thelma@gmail.com>
2024-06-13 18:42:34 +0100

    server

--- chat-app-backend/server.js
+++ chat-app-backend/server.js
@@ -1,38 +1,40 @@
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
-const mongoose = require("mongoose");
+// const express = require("express");
+// const http = require("http");
+// const socketIo = require("socket.io");
+// const mongoose = require("mongoose");
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+// const app = express();
+// const server = http.createServer(app);
+// const io = socketIo(server);
 
-io.on("connection", (socket) => {
-    console.log("New client connected");
+// io.on("connection", (socket) => {
+//     console.log("New client connected");
 
-    socket.on("disconnect", () => {
-        console.log("Client disconnected");
-    });
+//     socket.on("disconnect", () => {
+//         console.log("Client disconnected");
+//     });
 
-    socket.on("message", (message) => {
-        console.log("New message: " + message);
-        io.emit("message", message);
-    });
-});
+//     socket.on("message", (message) => {
+//         console.log("New message: " + message);
+//         io.emit("message", message);
+//     });
+// });
 
-const connectToDatabase = async () => {
-    try {
-        await mongoose.connect("mongodb://cluster0.lssixvh.mongodb.net/wire-chat-app");
-        console.log("MongoDB connected");
-    } catch (err) {
-        console.error("MongoDB connection error:", err);
-    }
-};
+// const connectToDatabase = async () => {
+//     try {
+//         await mongoose.connect("mongodb://cluster0.lssixvh.mongodb.net/wire-chat-app");
+//         console.log("MongoDB connected");
+//     } catch (err) {
+//         console.error("MongoDB connection error:", err);
+//     }
+// };
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// server.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
 
-connectToDatabase();
+// connectToDatabase();
+
+
