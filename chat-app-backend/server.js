db1a4a1c9350580f2e754544ebfa9005885b7d34
DCTP-BE <akpata.thelma@gmail.com>
2024-06-13 17:43:18 +0100

    server

--- chat-app-backend/server.js
+++ chat-app-backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const socketIo = require("socket.io");
 const http = require("http");
+const socketIo = require("socket.io");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -10,25 +10,29 @@ const io = socketIo(server);
 io.on("connection", (socket) => {
     console.log("New client connected");
 
-socket.on("disconnect", () => {
-    console.log("Client disconnected");
-});
-socket.on("message", (message) => {
-    console.log("New message: " + message);
-    io.emit("message", message);
-});
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+    });
+
+    socket.on("message", (message) => {
+        console.log("New message: " + message);
+        io.emit("message", message);
+    });
 });
 
-// mongoose.connect("mongodb://localhost:27017/chat-app", { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-//     console.log("MongoDB connected");
-// })
-// mongoose.connect('mongodb://localhost:27017/Tee', { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.connect('mongodb+srv://coursequesthub:fePziw-bewbaz-5cofme@cluster0.lssixvh.mongodb.net/wire-chat-app', { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-    console.log("MongoDB connected");
-});
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect("mongodb://cluster0.lssixvh.mongodb.net/wire-chat-app", { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("MongoDB connected");
+    } catch (err) {
+        console.error("MongoDB connection error:", err);
+    }
+};
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+connectToDatabase();
