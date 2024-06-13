80faa8fcdae1b182364e66c8566ba0bd699b274e
DCTP-BE <akpata.thelma@gmail.com>
2024-06-13 18:49:08 +0100

    server

--- chat-app-backend/server.js
+++ chat-app-backend/server.js
@@ -40,10 +40,12 @@
 
 const express = require("express");
 const app = express();
-const port = 7000;
+const port = 4000;
 
 app.get('/', (req, res) => {
     res.sendFile(`__dirname + '/index.html'`)
 });
 
-app.listen(port)
+app.listen(port, () => {
+    console.log(`CONECTED. Server is running on port ${port}`);
+})
