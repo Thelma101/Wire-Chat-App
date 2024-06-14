b0dd122fb257b4fae44f7233b4a0fed218bf0349
DCTP-BE <akpata.thelma@gmail.com>
2024-06-14 15:58:02 +0100

    app

--- chat-app-frontend/src/App.js
+++ chat-app-frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from ('react');
 import './App.css';
 import socketIoClient from 'socket.io-client';
+import Chat from './Chat';
 
 function App() {
   return (
     <div className="App">
+
+      <Chat />
 
     </div>
   );
