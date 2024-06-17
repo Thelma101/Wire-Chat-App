8eeab0bd137d45dbadd3994f44f554929bb54a5f
DCTP-BE <akpata.thelma@gmail.com>
2024-06-17 17:22:28 +0100

    app

--- chat-app-frontend/src/components/Login.jsx
+++ chat-app-frontend/src/components/Login.jsx
@@ -1,11 +1,11 @@
 function digitalRoot(n) {
-    if (n < 10) {
+    if (n >= 10) {
         
        const i = n.toString().split('').map(Number);
-        i.reduce((num, acc) => num + acc, 0);
-        return i;
-        
-    }
-
+        n = i.reduce((num, acc) => num + acc, 0);   
+    
+        return n;
+      }
 }
-console.log(digitalRoot(166));
+console.log(digitalRoot(195));
+console.log(digitalRoot(699993));
