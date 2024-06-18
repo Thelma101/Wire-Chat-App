5229233fd8fc897a23c5800408b60163a9a579e2
DCTP-BE <akpata.thelma@gmail.com>
2024-06-18 21:18:40 +0100

    app

--- chat-app-backend/codewars.js
+++ chat-app-backend/codewars.js
@@ -11,18 +11,19 @@
 // console.log(digitalRoot(195));
 // console.log(digitalRoot(699993));
 
-function ifChuckNorrisSaysSo() {
-    return !true;
-}
-// ifChuckNorrisSaysSo();
-console.log(ifChuckNorrisSaysSo());
+// function ifChuckNorrisSaysSo() {
+//     return !true;
+// }
+// // ifChuckNorrisSaysSo();
+// console.log(ifChuckNorrisSaysSo());
 
-function isPrime(num) {
-    for (let i = 2, s = Math.sqrt(num); i <= s; i++) {
-        if (num % i == 0) return false;
-    }
-    return num > 1;
-}
+// function isPrime(num) {
+//     for (let i = 2, s = Math.sqrt(num); i <= s; i++) {
+//         if (num % i == 0) return false;
+//     }
+//     return num > 1;
+// }
 
 // return isPrime(28);
-console.log(isPrime(2));
+// console.log(isPrime(2));
+
