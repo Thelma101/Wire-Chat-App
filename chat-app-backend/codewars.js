86899a4cd25e9638c25ec09a278d9d16797f579f
DCTP-BE <akpata.thelma@gmail.com>
2024-06-18 20:51:50 +0100

    app

---
+++ chat-app-backend/codewars.js
@@ -0,0 +1,17 @@
+// function digitalRoot(n) {
+//     if (n >= 10) {
+        
+//        const i = n.toString().split('').map(Number);
+//         n = i.reduce((num, acc) => num + acc, 0);   
+//         return digitalRoot(n);
+        
+//       }
+//       return n;
+// }
+// console.log(digitalRoot(195));
+// console.log(digitalRoot(699993));
+
+function ifChuckNorrisSaysSo() {
+    return !true;
+}
+ifChuckNorrisSaysSo();
