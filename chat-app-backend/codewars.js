f874cd72803059341fa42e38828889c2c15a201e
DCTP-BE <akpata.thelma@gmail.com>
2024-06-18 21:02:39 +0100

    app

--- chat-app-backend/codewars.js
+++ chat-app-backend/codewars.js
@@ -15,3 +15,10 @@ function ifChuckNorrisSaysSo() {
     return !true;
 }
 ifChuckNorrisSaysSo();
+
+function isPrime(num) {
+    for (let i = 2, s = Math.sqrt(num); i <= s; i++) {
+        if (num % i == 0) return false;
+    }
+    return num > 1;
+}
