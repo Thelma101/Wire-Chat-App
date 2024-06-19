3389b5ebf8122e4fefa1eec7db46a5c4cf448870
DCTP-BE <akpata.thelma@gmail.com>
2024-06-19 15:56:15 +0100

    app

--- chat-app-backend/codewars.js
+++ chat-app-backend/codewars.js
@@ -37,5 +37,5 @@ return isPrime(28);
 function listPrimeNumbers(primeNum) {
     for (i = 2, s = Math.sqrt(primeNum); i <= s; i++) { }
     if (i % i == 0);
-    console.log("false");
+    console.log(isPrime(i));
 }
