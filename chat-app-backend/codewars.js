7012de0a7aafedacd474ce56fefb345782ab70f1
DCTP-BE <akpata.thelma@gmail.com>
2024-06-20 18:00:23 +0100

    app

--- chat-app-backend/codewars.js
+++ chat-app-backend/codewars.js
@@ -38,5 +38,7 @@ function listPrimeNumbers(primeNum) {
     for (i = 2, s = Math.sqrt(primeNum); i <= s; i++) { 
     if (primeNum % i == 0);
 }
-console.log(isPrime(99))
+// console.log(isPrime(99))
 };
+
+console.log(listPrimeNumbers(99));
